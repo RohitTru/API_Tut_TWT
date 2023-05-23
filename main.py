65dfa51de5580060029e0d61f02e62b817aa67b1
Rohit <rohit.truesdale@gmail.com>
2023-05-23 16:37:19 -0500

    created a post route

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify
 # Initializing Flask
 app = Flask(__name__)
 
-# A more realistic route
+# A Get route Example;
 @app.route('/get-user/<user_id>') # after the second '/' we initialize something known as a path parameter which allows us to dynamically pass in a value that we can then access in the route
 def get_user(user_id):
     # example data:
@@ -16,6 +16,13 @@ def get_user(user_id):
         user_data["extra"] = extra
     
     return jsonify(user_data), 200 # jsonify: javascript object notation which is essentially a python dictionary in javascript. This allows a flask to parse our dictionary and return it in json to show the user. The 200 we pass is a status code that means success
+
+# A post route:
+@app.route('/create-user', methods =["POST"])
+def create_user():
+    data = request.get_json()
+    
+    return jsonify(data), 201
 
 # Running our Flask Application
 if __name__ == "__main__":
