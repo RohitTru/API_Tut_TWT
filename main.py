60987f2bd35d67c5f853951535e5134a0570f678
Rohit <rohit.truesdale@gmail.com>
2023-05-23 16:19:22 -0500

    better example route and set some example data

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from flask import Flask, request, jsonify
 # Initializing Flask
 app = Flask(__name__)
 
+# A more realistic route
+@app.route('/get-user/<user_id>') # after the second '/' we initialize something known as a path parameter which allows us to dynamically pass in a value that we can then access in the route
+def get_user(user_id):
+    # example data:
+    user_data = {"user_id" : user_id,
+                 "name": "Rohit Truesdale",
+                 "email": "rohit.trues@gmail.com"}
 
-# Test endpoint
-@app.route("/")
-def home():
-    return "Home"
 
 # Running our Flask Application
 if __name__ == "__main__":
